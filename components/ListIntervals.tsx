d78a907d91d89da9ad76024589caf5135992a55e
Anushree <anushreejaiswal.srn@gmail.com>
2023-07-22 05:14:38 +0530

    added remove interval button

--- components/ListIntervals.tsx
+++ components/ListIntervals.tsx
@@ -7,7 +7,7 @@ const rajdhani = Rajdhani({
     weight: "500"
 })
 
-const ListIntervals = ({addInterval}: any) => {
+const ListIntervals = ({addInterval, removeInterval}: any) => {
     return (
         <>
             {intervals.map((currentValue: string, index: number, key: any) =>
@@ -20,6 +20,7 @@ const ListIntervals = ({addInterval}: any) => {
                     >
                         <input 
                         type="time"
+                        defaultValue={"08:00"}
                         className={`border-2 border-white text-black text-[24px] font-semibold rounded-lg py-[3px] px-4 m-1 focus:outline focus:outline-2 focus:outline-[#48d399] focus:border-[#48d399] focus:bg-transparent focus:text-white ${rajdhani.className}`}
                         />
 
@@ -29,15 +30,28 @@ const ListIntervals = ({addInterval}: any) => {
 
                         <input 
                         type="time"
+                        defaultValue={"10:00"}
                         className={`border-2 border-white text-black text-[24px] font-semibold rounded-lg py-[3px] px-4 m-1 focus:outline focus:outline-2 focus:outline-[#48d399] focus:border-[#48d399] focus:bg-transparent focus:text-white ${rajdhani.className}`}
                         />
 
-                        <button
-                        className="w-[35px] h-[35px] rounded-[25px] ml-[20px]"
-                        onClick={() => addInterval()}
-                        >
-                            <Image width="60" height="60" src="https://img.icons8.com/48d399/doodle/96/000000/add.png" alt="add" />
-                        </button>
+                        <div>
+                            <button
+                            className="w-[35px] h-[35px] rounded-[25px] ml-[20px]"
+                            onClick={() => addInterval()}
+                            >
+                                <Image width="60" height="60" src="https://img.icons8.com/48d399/doodle/96/000000/add.png" alt="add" />
+                            </button>
+
+                            {intervals.length > 1 ? 
+                                <button
+                                className="w-[35px] h-[35px] rounded-[25px] ml-[15px]"
+                                onClick={() => removeInterval()}
+                                >
+                                    <Image width="60" height="60" src="https://img.icons8.com/color/96/delete-forever.png" alt="remove" />
+                                </button> :
+                                <button className="w-[35px] h-[35px] rounded-[25px] ml-[15px] z-[-1]"></button>
+                            }
+                        </div>                        
                     </div>
                 </div>
             )}
