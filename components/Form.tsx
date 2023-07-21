ab3f7d2f3463502a6dcef0947c71bb4823a9d01f
Anushree <anushreejaiswal.srn@gmail.com>
2023-07-22 03:35:03 +0530

    replace button position and updated the input styles

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react"
 import ListIntervals from "./ListIntervals"
 import { intervals, timeIntervals } from "@/lib/utils"
-import Image from "next/image"
 
 const Form = () => {
     const [intLength, setIntLength] = useState<number>(intervals.length)
@@ -14,7 +13,7 @@ const Form = () => {
     const renderForm = () => {
         return (
             <form
-            className='w-[350px] border-2 border-white text-white text-xl p-4 flex flex-col justify-center items-center bg-[#16161a] shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'
+            className='w-[350px] border-2 border-white rounded-2xl text-white text-xl p-4 flex flex-col justify-start items-center bg-[#16161a]'
             onSubmit={(e) => {
                 e.preventDefault()
             }}
@@ -24,21 +23,18 @@ const Form = () => {
                 name="name" 
                 id="my-name" 
                 type="text" 
-                className='placeholder:text-slate-300 w-full mb-4 p-2 border-2 border-white bg-transparent rounded-lg focus:outline focus:outline-2 focus:outline-[#48d399] focus:border-[#48d399]' 
+                className='placeholder:text-slate-300 w-full p-2 mb-6 border-2 border-white bg-transparent rounded-lg focus:outline focus:outline-2 focus:outline-[#48d399] focus:border-[#48d399]' 
                 placeholder='name' />
 
-                <ListIntervals />
+                <ListIntervals addInterval={addInterval} />
 
                 <button
-                className="w-[50px] h-[50px] rounded-[25px]"
-                onClick={() => addInterval()}
-                >
-                    <Image width="60" height="60" src="https://img.icons8.com/doodle/96/add.png" alt="add" />
-                </button>
-
-                <button
-                className="m-4 py-2 px-5 border-2 border-white rounded-lg font-medium hover:bg-white hover:font-semibold hover:text-[#16161a]"
+                className="m-4 mb-6 py-2 px-5 border-2 border-white rounded-lg font-medium hover:bg-white hover:font-semibold hover:text-[#16161a]"
                 >Submit</button>
+
+                <button
+                className="m-4 mb-6 py-2 px-5 border-2 border-white rounded-lg font-medium hover:bg-white hover:font-semibold hover:text-[#16161a]"
+                >Results</button>
             </form>
         )
     }
--- components/ListIntervals.tsx
+++ components/ListIntervals.tsx
@@ -1,6 +1,13 @@
-import { intervals, timeIntervals } from "@/lib/utils"
+import { intervals } from "@/lib/utils"
+import Image from "next/image"
+import { Rajdhani } from 'next/font/google'
 
-const ListIntervals = () => {
+const rajdhani = Rajdhani({
+    subsets: ['latin'],
+    weight: "500"
+})
+
+const ListIntervals = ({addInterval}: any) => {
     return (
         <>
             {intervals.map((currentValue: string, index: number, key: any) =>
@@ -13,13 +20,24 @@ const ListIntervals = () => {
                     >
                         <input 
                         type="time"
-                        className="border-2 border-white text-black rounded-lg py-1 px-4 m-1"
+                        className={`border-2 border-white text-black text-[24px] font-semibold rounded-lg py-[3px] px-4 m-1 focus:outline focus:outline-2 focus:outline-[#48d399] focus:border-[#48d399] focus:bg-transparent focus:text-white ${rajdhani.className}`}
                         />
-                        <span>-</span>
+
+                        <span
+                        className="font-semibold text-2xl"
+                        >-</span>
+
                         <input 
                         type="time"
-                        className="border-2 border-white text-black rounded-lg py-1 px-4 m-1"
+                        className={`border-2 border-white text-black text-[24px] font-semibold rounded-lg py-[3px] px-4 m-1 focus:outline focus:outline-2 focus:outline-[#48d399] focus:border-[#48d399] focus:bg-transparent focus:text-white ${rajdhani.className}`}
                         />
+
+                        <button
+                        className="w-[35px] h-[35px] rounded-[25px] ml-[20px]"
+                        onClick={() => addInterval()}
+                        >
+                            <Image width="60" height="60" src="https://img.icons8.com/48d399/doodle/96/000000/add.png" alt="add" />
+                        </button>
                     </div>
                 </div>
             )}
