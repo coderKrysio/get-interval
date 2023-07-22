51aa0aa1f3122640c9e4091367af91ffe34dbeaa
Anushree <anushreejaiswal.srn@gmail.com>
2023-07-22 23:08:17 +0530

    updated the timeline

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,34 +1,50 @@
 import { rajdhani } from "@/lib/fonts";
 
 const timeline: number[] = [];
-for(let x = 0; x < 24; x++) {
+for(let x = 0; x < 24 * 60; x++) {
     timeline[x] = x;
 }
+
 const Timeline = () => {
+    const blockLine = (result: boolean, time: number) => {
+        const color: string = result ? '#48d399' : '#db696d';
+        return (
+            <div
+            className={`py-[20px] mx-[-7px] relative flex justify-start border-t-4 border-[${color}]`}
+            >
+                <span
+                className={`before:content-[''] before:w-[3px] before:h-[25px] before:bg-[${color}] before:absolute before:top-[-13px] before:left-0 before:border-4 before:border-[${color}] font-semibold text-[${color}] text-xl`}
+                >{time}</span>
+            </div>
+        )
+    }
+
+    const conditionMet = (time: number) => {
+        return time >= 14 && time <= 15 || time >= 18 && time <= 23
+    }
+
     return (
         <div
-        className="my-[50px] flex p-0 text-center border-[1px] border-white"
+        className="my-[50px] flex p-0 text-center"
         >
             {timeline.map((time: number, index: number) =>
-                <div key={index}
-                className={`w-[60px] text-center text-lg ${rajdhani.className}`}
+                <div 
+                key={index}
+                className={`w-[1px] text-center text-lg ${rajdhani.className}`}
                 >
-                    {time >= 14 && time < 15 || time >= 18 && time <= 23 ?
-                        <div
-                        className="py-[20px] relative flex justify-start border-t-4 border-[#48d399]"
-                        >
-                            <span
-                            className="before:content-[''] before:w-[3px] before:h-[25px] before:bg-[#48d399] before:absolute before:top-[-13px] before:left-0 before:border-4 before:border-[#48d399] before:z-[1] font-semibold text-[#48d399] text-xl"
-                            >{time}</span> 
-                        </div>
+                    {time % 60 == 0 ?
+                        blockLine(conditionMet(time / 60), time / 60)
                         :
-                        <div
-                        className="py-[20px] relative flex justify-start border-t-4 border-[#db696d]"
-                        >
-                            <span
-                            className="before:content-[''] before:w-[3px] before:h-[25px] before:bg-[#db696d] before:absolute before:top-[-13px] before:left-0 before:border-4 before:border-[#db696d] font-semibold text-[#db696d] text-xl"
-                            >{time}</span>
-                        </div>
+                        <>
+                            {conditionMet(time / 60) ? 
+                                <div
+                                className="py-[20px] relative flex justify-start border-t-4 border-[#48d399]"
+                                ></div>
+                                : <div
+                                className="py-[20px] relative flex justify-start border-t-4 border-[#db696d]"
+                                ></div>
+                            }
+                        </>
                     }
                 </div>
             )}
