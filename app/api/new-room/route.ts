8eb5afd55408981f261d143bff33620b78885df7
Ishank Lalwani <ishanklalwani@gmail.com>
2023-07-24 20:43:20 +0530

    minor changes

--- app/api/new-room/route.ts
+++ app/api/new-room/route.ts
@@ -4,17 +4,11 @@ import clientPromise from "@/utils/newdb";
 export async function POST(request: Request) {
     const {username, roomcode, timeRanges} = await request.json();
     try {
-      //await connectToDB()
       const client = await clientPromise;
       const db = client.db("get_interval");
-      
-      const newRoom = new Room({
-        username:username,
-        roomcode:roomcode,
-        timeRanges:timeRanges
-      });
 
       await db.collection("rooms").insertOne({"username":username, "roomcode":roomcode, "timeRanges":timeRanges});
+      
       console.log("New Room Created!");
       return new Response ("Success!", {status:200}); 
     } catch (error) {
