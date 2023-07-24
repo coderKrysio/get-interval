1e13c15ce3cd632bcd5e0f2b8f07c71786e8b0c7
Ishank Lalwani <ishanklalwani@gmail.com>
2023-07-24 09:47:22 +0530

    get request done

--- app/api/[roomcode]/route.ts
+++ app/api/[roomcode]/route.ts
@@ -1,22 +1,69 @@
-import Room from "@/models/room";
-import { connectToDB } from "@/utils/database";
+import clientPromise from "@/utils/newdb";
+import z from "zod";
 
-export async function POST(request: Request) {
-    const {username, roomcode, timeRanges} = await request.json();
+
+type RoomCodeRouteContext =  {
+  params: {
+    roomcode: string,
+  }
+}
+
+export async function GET(request:Request,  context: RoomCodeRouteContext){
     try {
-      await connectToDB();
+      const client = await clientPromise;
+      const db = client.db("get_interval");
       
-      const newRoom = new Room({
-        username:username,
-        roomcode:roomcode,
-        timeRanges:timeRanges
-      });
+      const rooms = await db.collection("rooms").find({roomcode:context.params.roomcode}).limit(10).toArray();
+      
+      console.log(rooms);
+      
+      //to get all the intervals in a single array
+      let intervals:any[] = [[]];
+      let counter = 0;
+      for(let i=0;i<rooms.length;i++){
+        var room=rooms[i].timeRanges;
+        for(let j=0;j<room.length;j++){
+          intervals[counter] = room[j];
+          counter++;
+        }
+      }
 
-      await newRoom.save();
-      console.log("New Room Created!");
-       
+      //to sort the array 
+        for (let i = 0; i < intervals.length-1; i++) {
+            for (let j = 0; j < intervals.length-1-i; j++) {
+                if(intervals[j][0]>intervals[j+1][0]){
+                    let t=intervals[j];
+                    intervals[j]=intervals[j+1];
+                    intervals[j+1]=t;
+                }
+            }
+        }
+
+        //to merge the intervals
+        let finalintervals : any[];
+        finalintervals = [[intervals[0][0], intervals[0][1]]]
+        for(let i=0;i<intervals.length;i++){
+          if(finalintervals[finalintervals.length -1][1]>=intervals[i][0]){
+            finalintervals[finalintervals.length-1][1]= Math.max(finalintervals[finalintervals.length-1][1],intervals[i][1]);
+          }
+          else{
+            finalintervals.push(intervals[i]);
+          }
+        }
+
+      // console.log(finalintervals);
+
+      //to get the free intervals
+      let result : any[];
+      result = [[0, finalintervals[0][0]]];
+      for(let i=1;i<finalintervals.length;i++){
+        result[i]=[finalintervals[i-1][1]+1,finalintervals[i][0]+1];
+      }
+      result[result.length]=[finalintervals[finalintervals.length-1][1]+1, 1440];
+      // console.log(result);
+
+      return new Response(JSON.stringify(result), {status:200});
     } catch (error) {
-        console.log(error);
-        return new Response("Failed to create a new room, try again later!", {status:500});
+        return new Response("Failed to fetch the data, Try again later!!", {status:500});
     }
 }
