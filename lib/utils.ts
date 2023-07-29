27fd622c50adb174f40afa16beebc90ca2c3fc88
Anushree <anushreejaiswal.srn@gmail.com>
2023-07-29 18:28:21 +0530

    updated imports

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,65 +7,6 @@ export const timeIntervals = [480, 600]
 
 export const intervals: number[][] = [timeIntervals]
 
-export const membersData = [
-    {
-        id: 0,
-        roomCode: 'ahd-kjha-hjah',
-        name: 'Anushree',
-        intervals: [
-            [0, 839],
-            [901, 1079],
-            [1381, 1439],
-        ],
-    },
-    {
-        id: 1,
-        roomCode: 'ahd-kjha-hjah',
-        name: 'Ishank',
-        intervals: [
-            [0, 659],
-            [901, 1259],
-        ],
-    },
-    {
-        id: 2,
-        roomCode: 'ahd-kjha-hjah',
-        name: 'Shubh',
-        intervals: [
-            [0, 540],
-            [750, 839],
-        ],
-    },
-    {
-        id: 0,
-        roomCode: 'ahd-kjha-hjah',
-        name: 'Anushree',
-        intervals: [
-            [0, 839],
-            [901, 1079],
-            [1381, 1439],
-        ],
-    },
-    {
-        id: 1,
-        roomCode: 'ahd-kjha-hjah',
-        name: 'Ishank',
-        intervals: [
-            [0, 659],
-            [901, 1259],
-        ],
-    },
-    {
-        id: 2,
-        roomCode: 'ahd-kjha-hjah',
-        name: 'Shubh',
-        intervals: [
-            [0, 540],
-            [750, 839],
-        ],
-    },
-]
-
 export function NumToTime(num: number) {
     var hours = Math.floor(num / 60).toString()
     var minutes: string = (num % 60).toString()
