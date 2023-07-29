299003d87205c057f3b398739614b36255d7fad0
Ishank Lalwani <ishanklalwani@gmail.com>
2023-07-29 13:33:11 +0530

    delete many used

--- app/api/[roomcode]/delete/route.ts
+++ app/api/[roomcode]/delete/route.ts
@@ -4,17 +4,9 @@ import clientPromise from '@/utils/newdb'
 export async function DELETE(_request: Request, context: RoomCodeRouteContext) {
     try {
         const client = await clientPromise
-        const db = client.db('get_interval')
+        const db = client.db('get_interval');
 
-        const rooms = await db
-            .collection('rooms')
-            .find({ roomcode: context.params.roomcode })
-            .limit(10)
-            .toArray()
-
-        for (let i = 0; i < rooms.length; i++) {
-            db.collection('rooms').findOneAndDelete({ _id: rooms[i]._id })
-        }
+        db.collection('rooms').deleteMany({ roomcode: context.params.roomcode })
 
         return new Response(JSON.stringify(''), { status: 200 })
     } catch (error) {
