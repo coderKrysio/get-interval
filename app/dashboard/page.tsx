2146900005e7797c4ccc70ba307ab7486eb20bba
Anushree <anushreejaiswal.srn@gmail.com>
2023-08-05 07:19:32 +0530

    added middleware

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+import { Dashboard } from "@/components/dashboard/DashBoard";
+
+export default function Page() {
+    return (
+        <Dashboard />
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 'use client'
 import {LandingPage} from '@/components/LandingPage'
-import { Dashboard } from '@/components/dashboard/DashBoard'
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center bg-[#000] overflow-x-hidden">
-            <Dashboard />
+            <LandingPage />
         </main>
     )
 }
