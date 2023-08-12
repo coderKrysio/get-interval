81a2b5916ca4c6e95bf51a4c667ac6cc27ee8c16
Ishank Lalwani <ishanklalwani@gmail.com>
2023-08-12 20:18:51 +0530

    added user signin check

--- app/api/[roomcode]/delete/route.ts
+++ app/api/[roomcode]/delete/route.ts
@@ -1,18 +1,27 @@
-import { RoomCodeRouteContext } from '@/lib/types'
-import clientPromise from '@/utils/newdb'
+import { RoomCodeRouteContext } from '@/lib/types';
+import clientPromise from '@/utils/newdb';
+import { auth } from '@clerk/nextjs';
 
 export async function DELETE(_request: Request, context: RoomCodeRouteContext) {
     try {
-        const client = await clientPromise
-        const db = client.db('get_interval')
+        const userId = auth();
 
-        db.collection('rooms').deleteMany({ roomcode: context.params.roomcode })
+        if (!userId) {
+            return new Response('Unauthenticated!', { status: 403 });
+        }
 
-        return new Response(JSON.stringify(''), { status: 200 })
+        const client = await clientPromise;
+        const db = client.db('get_interval');
+
+        db.collection('rooms').deleteMany({
+            roomcode: context.params.roomcode,
+        });
+
+        return new Response(JSON.stringify(''), { status: 200 });
     } catch (error) {
-        console.log(error)
+        console.log(error);
         return new Response('Failed to delete, Try again later!!', {
             status: 500,
-        })
+        });
     }
 }
